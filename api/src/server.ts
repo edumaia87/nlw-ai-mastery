84771409cab18948350b0b1c701458d0e2f7e9f7
Eduardo Maia <edumaia89@outlook.com>
2023-09-12 22:37:22 -0300

    feat:create prisma client and first route

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,10 +1,13 @@
 import { fastify } from 'fastify'
+import { prisma } from './lib/prisma'
 
 const app = fastify()
 
-app.get('/', () => {
-  return 'Hello, world'
-})
+app.get('/prompts', async () => {
+  const prompts = await prisma.prompt.findMany()
+
+  return prompts
+})  
 
 app.listen({
   port: 3333,
